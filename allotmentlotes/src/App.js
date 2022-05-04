54c6c7d2c22e6af7763658a4beda049337cde03a
guilhermeSantos <guilhermeigatu99@gmail.com>
2022-05-04 19:59:39 -0300

    AppJS confiigurado

--- allotmentlotes/src/App.js
+++ allotmentlotes/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+/* Import para utiização do State e useEffect */
+import { useState, useEffect } from "react";
+/* Componentização do elementos da página */
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/sobre";
+import { Services } from "./components/servicos";
+import { Portfolio } from "./components/portfolio";
+import { Contact } from "./components/contato";
+/* Importação do data em Json*/
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
 
-function App() {
+
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+
+/* Setando estados vindo do DATA.Json */
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation                                         />
+      <Header       data={landingPageData.Header}         />     
+      <Features     data={landingPageData.Features}       />
+      <About        data={landingPageData.Sobre}          />
+      <Services     data={landingPageData.Servicos}       />
+      <Portfolio    data={landingPageData.Portfolio}      />
+      <Contact      data={landingPageData.Contato}        />
+
     </div>
   );
-}
+};
 
 export default App;
